9b7c99a7e5ac146ecf41ab8b42e08a560186cae6
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-14 11:04:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,23 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  post "/articles" do
+    article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  get "/articles/:id" do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
 end
